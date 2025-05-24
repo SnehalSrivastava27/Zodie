a752402be6b7357f7a11b50fff072ac3066e0c37
Snehal Srivastava <118104081+SnehalSrivastava27@users.noreply.github.com>
2025-05-24 22:01:40 +0530

    Initial pages done

---
+++ frontend/src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HeroSection } from './components/blocks/hero-section-2'
+import { LandingPage } from './Page/LandingPage'
+import { PrimeHrPage } from './Page/PrimeHrAgent'
+import { OptimusPage } from './Page/OptimusAgent'
+import { NovaPage } from './Page/NovaAgent'
+import { MaxiPage } from './Page/MaxiAgent'
+import { ArchiePage } from './Page/ArchieAgent'
+import { OnixPage } from './Page/OnixAgent'
+
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage/>} />
+        <Route path='/prime' element={<PrimeHrPage/>}/>
+        <Route path='/optimus' element={<OptimusPage/>}/>
+        <Route path='/nova' element={<NovaPage/>}></Route>
+        <Route path='/maxi' element={<MaxiPage/>}></Route>
+        <Route path='/archie' element={<ArchiePage/>}/>
+        <Route path='/onix' element={<OnixPage/>}/>
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
